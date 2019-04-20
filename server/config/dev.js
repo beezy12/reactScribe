301a01656b19124b4ad5468bce4a4f281b615fda
Brian Stumbaugh <brian.stumbaugh@gmail.com>
2019-04-20 10:49:31 -0500

    Delete dev.js

--- server/config/dev.js
+++
@@ -1,6 +0,0 @@
-// dev.js -------> DO NOT COMMIT THIS
-
-module.exports = {
-  coinmarketcapSandboxKey: '1997133c-09b3-4eff-8036-84f47991c1cb',
-  mongoURI: 'mongodb://beezycryptotrackeradmin:cryptotrackeradminpassword1@ds157574.mlab.com:57574/cryptotracker'
-}
