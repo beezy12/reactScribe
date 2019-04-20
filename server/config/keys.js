f26ac24c9e3580ad500a6e92d488c510e3375eef
Brian Stumbaugh <brian.stumbaugh@gmail.com>
2019-04-20 11:40:05 -0500

    delete keys.js

--- server/config/keys.js
+++
@@ -1,8 +0,0 @@
-if (process.env.NODE_ENV === 'production') {
-  // return production keys
-  module.exports = require('./prod');
-} else {
-  // return dev keys
-  // I am exporting and requiring in the dev file at the same time
-  module.exports = require('./dev');
-}
